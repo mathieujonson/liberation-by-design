8a5ea1b1c8b017ec089efd35dae2e50ace81f233
Andrew Strawbridge <ajstraw4002@gmail.com>
2018-02-10 10:38:43 -0600

    Added login in function and react bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home'
 import HowToPlay from './components/HowToPlay/HowToPlay'
 import Definitions from './components/Definitions/Definitions'
 import Play from './components/Play/Play'
+import Login from './components/Admin/Login'
 import NotFound from './components/NotFound'
 import Footer from './components/Footer'
 import './styles/main.css'
@@ -21,6 +22,7 @@ class App extends Component {
                             <Route path="/how-to-play" component={HowToPlay} />
                             <Route path="/our-definitions" component={Definitions} />
                             <Route path="/play" component={Play} />
+                            <Route path="/admin" component={Login} />
                             <Route component={NotFound} />
                         </Switch>
                     </div>
