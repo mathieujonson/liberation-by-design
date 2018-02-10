bb1c5e76ed7ef2d6c5820fa7ecca23867f6dbbc1
Matt Johnson <mzjohnson@caleres.com>
2018-02-10 12:29:23 -0600

    questions changes

---
+++ src/components/Admin/Questions/index.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {getQuestions} from '../../actions/definitions';
+
+
+class Definitions extends Component {
+    componentWillMount() {
+        this.props.getDefinitions()
+        document.title = 'Our Definitions - Liberation By Design'
+
+    }
+
+    render() {
+        let list = ''
+
+        if(Object.keys(this.props.definitions).length > 0) {
+            list = this.props.definitions.definitions.map((definition, index) => {
+                return (
+                    <li key={index}><strong>{definition.term}</strong>: {definition.definition}</li>
+                )
+            })
+        }
+
+        console.log(list)
+
+        return (
+            <div className="our-definitions-container">
+                <h1>Our Definitions</h1>
+                <ul>
+                    {list}
+                </ul>
+                <Link to='/play' className="button">Let's Play!</Link>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        definitions: state.definitions,
+        state: state,
+        inProgress: state.definitions.inProgress
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getDefinitions: () => dispatch(getDefinitions())
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Definitions);
