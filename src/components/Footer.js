3949286de29697ca9ed52f8b94f392a28ca7f4c5
Jennifer Young <jen.l.young@gmail.com>
2018-02-10 20:22:04 -0600

    Feat: add footer elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,46 @@
 import React from 'react'
+// import {Link, NavLink} from 'react-router-dom'
+
 
 export default (props) => (
-  <footer className="page-footer">
-    Here is some footer stuffs
-  </footer>
+    <footer className="page-footer">
+        <section className="footer">
+            <ul className="footer-buttons">
+                <li><a href="http://www.creativereactionlab.com/upcoming-events"
+                >upcoming events</a></li>
+                <li><a href="http://www.creativereactionlab.com/partnership-interest-form
+">becoming a partner</a></li>
+                <li><a href="http://www.creativereactionlab.com/subscribe">subscribe</a></li>
+                <li><a href="http://www.creativereactionlab.com/donate">donate</a></li>
+                <li><a href="http://www.creativereactionlab.com/contact">contact us</a></li>
+            </ul>
+        </section>
+        <div className="copyright">
+            <p>&copy; Creative Labs | 2018</p>
+            <h3>CREATIVE REACTION LAB IS A REGISTERED 501C3 NONPROFIT ORGANIZATION.</h3>
+        </div>
+        <section className="social-media">
+            <ul className="media-buttons">
+                <li><a href="https://www.facebook.com/CreativeReactionLab
+">facebook</a></li>
+                <li><a href="https://twitter.com/CreativeRxlab">twitter</a></li>
+                {/*https://twitter.com/CreativeRxlab*/}
+                <li><a href="https://www.linkedin.com/company/creative-reaction-lab">linked in</a></li>
+                {/*https://www.linkedin.com/company/creative-reaction-lab*/}
+            <li><a href="mailto:acarroll@creativereactionlab.com
+">mail</a></li>
+                {/*mailto:acarroll@creativereactionlab.com*/}
+                <li><a href="https://www.instagram.com/crxlab/
+">instagram</a></li>
+                {/*https://www.instagram.com/crxlab/*/}
+            </ul>
+        </section>
+    </footer>
+
 )
+
+// <li><NavLink exact to='/' activeClassName="active">Home</NavLink></li>
+
+
+
+    // <a href="mailto:jen@sharklasers.com"><img class="navPic" src="img/maillogo.png" alt="Email"></a>
