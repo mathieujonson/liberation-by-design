e0f7d242ec724b63b870934b451219470091a712
Tammy Ritterskamp <tritterskamp@gmail.com>
2018-02-11 11:08:06 -0600

    Adds in Facebook sharing, styling Summary page

--- src/components/Play/Summary.js
+++ src/components/Play/Summary.js
@@ -22,9 +22,19 @@ class Summary extends Component {
     }
 
     componentDidMount() {
-        document.title = 'Summary - Liberation By Design'
+        document.title = 'Summary - Liberation By Design';
+
+        //load the JavaScript SDK
+        (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     }
 
+        
     render() {
 
         if (typeof this.props.userInProgress == 'undefined' || this.props.userInProgress) {
@@ -32,28 +42,35 @@ class Summary extends Component {
         }
 
         const allQuestions = this.props.user.questions;        
-        let questionResponse = Object.keys(allQuestions).map(key =>
+        const questionSummary = Object.keys(allQuestions).map(key =>
         { 
-            allQuestions[key]
+            const questionIcon = allQuestions[key].correct ? `fa fa-check` : `fa fa-times`
+            return (
+                <li key={key}><i className={questionIcon}></i> <span className="summary__points">{allQuestions[key].points} points</span></li>
+            )
          }
         )
 
         return (
             <div className="summary">
+                <div id="fb-root"></div>
                 <div className="summary__text">
                     <h1>Thanks for playing!</h1>
                     <p>You got {this.props.user.score} points.</p>
                 </div>
-                <div class="summary__scores">
+                <div className="summary__scores">
                     <p>Game summary:</p>
                     <ol>
-                        <li>{questionResponse}</li>
+                        {questionSummary}
                     </ol>
                 </div>
                 <div className="summary__share">
-                    Share this game with a friend!
+                    <p>Share this game with a friend!</p>
+                    {/* Facebook */}
+                    <div className="fb-share-button" data-href="https://liberation-by-design.firebaseapp.com/" data-layout="button" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fliberation-by-design.firebaseapp.com%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a></div>
                 </div>            
                 <div className="survey-container">
+                    <p>Before you go, please take our short survey!</p>
                     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeleRf1hZPZzzx2UoTpkjb-3PSRi1ngGqBEffpiu2LrSSHGxQ/viewform?embedded=true" width="700" height="500" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>                
                 </div>
             </div>
